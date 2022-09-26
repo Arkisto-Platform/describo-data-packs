68fe6eabc2cf0b9a533f5a60e1b8ef48400d3ae7
Marco La Rosa <m@lr.id.au>
2022-09-26 14:51:07 +1000

    add some logging

--- update_all_packages.sh
+++ update_all_packages.sh
@@ -3,24 +3,30 @@
 cd languages
 
 # update Austlang data pack
+echo "Getting Austlang Data"
 cd Austlang-language-data
 node create-language-data-set.js
 cd ..
 
 # update Glottolog dataset
+echo "Getting Glottolog Data"
 cd Glottolog-language-data
 node create-language-data-set.js
 cd ..
 
 # update ethnologue dataset
+echo "Getting Ethnologue Data"
 cd ISO-639-3-language-data
 node create-language-data-pack.js
+cd ..
 
 # crossreference austlang and glottolog data packs and
+echo "Cross referencing language datasets"
 node cross-reference-languages.js
 cd ..
 
 # update country data set
+echo "Getting Country Data"
 cd countries/ISO-639-6-country-data
 node create-country-data-pack
 cd ..
