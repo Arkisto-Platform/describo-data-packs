113ff14e31db34b1745ba989678e4ae79b050a8f
Marco La Rosa <m@lr.id.au>
2022-09-26 15:34:45 +1000

    refactor and collapse into one

--- countries/ISO-639-6-country-data/create-country-data-pack.js
+++ countries/ISO-639-6-country-data/create-country-data-pack.js
@@ -1,32 +1,30 @@
-const data = "https://www.ethnologue.com/sites/default/files/CountryCodes.tab";
+const countryCodesTab = "https://www.ethnologue.com/sites/default/files/CountryCodes.tab";
+const countryGeoJSON = "https://datahub.io/core/geo-countries/r/countries.geojson";
 const languagePack = "./iso-639-6-country-data-pack.json";
+const countryISOMapping = require("./mapping-iso3-iso2.json"); // from https://github.com/vtex/country-iso-3-to-2
 const fetch = require("cross-fetch");
 const { writeJson } = require("fs-extra");
+const { groupBy } = require("lodash");
+const iso2to3Mapping = flipIsoMapping();
 
-(async () => {
-    let response = await fetch(data, { cache: "reload" });
-    if (response.status !== 200) {
-        throw new Error(response);
-    }
-    response = await response.text();
+main();
+async function main() {
+    let countryCodes = await get({ url: countryCodesTab });
+    const lines = countryCodes.split("\n").map((l) => l.trim());
 
-    const countryData = [];
-    for (let line of response.split("\n")) {
-        if (line.match("CountryID")) continue;
-
+    let countryData = [];
+    for (let line of lines.slice(1, lines.length)) {
         let components, code, country, name;
         try {
-            components = line.split("\t");
-            code = components.shift();
-            name = components.shift();
-            region = components[0].replace("\r", "");
+            let [code, name, region] = line.split("\t");
             if (name && code) {
                 // console.log(code, country, name);
                 countryData.push({
                     "@id": `https://www.ethnologue.com/country/${code}`,
-					"@type": "Country",
+                    "@type": "Country",
                     name,
-                    alternateName: code,
+                    iso2: code,
+                    iso3: iso2to3Mapping[code],
                 });
             }
         } catch (error) {
@@ -34,5 +32,42 @@ const { writeJson } = require("fs-extra");
         }
     }
 
+    let countryGeoJSONData = await get({ url: countryGeoJSON, as: "json" });
+    countryGeoJSONGroupedByName = groupBy(countryGeoJSONData.features, (c) => c.properties.ISO_A3);
+
+    countryData = countryData.map((country) => {
+        try {
+            country = {
+                ...country,
+                geojson: {
+                    "@id": "#" + country.name,
+                    "@type": "GeoShape",
+                    geojson: JSON.stringify(countryGeoJSONGroupedByName[country.iso3][0]),
+                },
+            };
+        } catch (error) {
+            console.warn(`No coordinate data found for: ${country.name}`);
+        }
+        return country;
+    });
+
     await writeJson(languagePack, countryData);
-})();
+}
+
+async function get({ url, as = "text" }) {
+    let response = await fetch(url, { cache: "reload" });
+    if (response.status !== 200) {
+        throw new Error(response);
+    }
+    if (as === "text") {
+        return await response.text();
+    } else if (as === "json") {
+        return await response.json();
+    }
+}
+
+function flipIsoMapping() {
+    let mapping = {};
+    Object.keys(countryISOMapping).forEach((key) => (mapping[countryISOMapping[key]] = key));
+    return mapping;
+}
